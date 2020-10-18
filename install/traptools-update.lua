544fb979917321f1ddfab10e76a73d43830a8dee
Guillaume ARM <trapcodien@gmail.com>
2020-10-18 22:23:21 +0200

    feat: add media package

---
+++ install/traptools-update.lua
@@ -0,0 +1,21 @@
+local exec = require('shell').execute
+
+-- Packages list
+local packageList = {
+  "traptools",
+  "openos-patches",
+  "libui",
+  "libui-demo",
+  "redstone-onoff",
+  "media",
+  "wd"
+}
+
+-- Uninstall all packages
+for k, v in pairs(packageList) do
+  exec('oppm uninstall ' .. v)
+end
+
+-- Reinstall traptools
+exec('oppm install -f traptools')
+exec('traptools-install')
