9226c3200d0ec4c55454b0c640cd62686d64d4ac
Guillaume ARM <trapcodien@gmail.com>
2020-10-24 10:39:18 +0200

    feat: improve traptools-update cli

--- install/traptools-update.lua
+++ install/traptools-update.lua
@@ -1,6 +1,14 @@
 local exec = require('shell').execute
 
+local firstArg, secondArg = ...
+
+local path = firstArg or '/'
+
 -- Reinstall traptools
 exec('traptools-uninstall')
-exec('oppm install -f traptools')
-exec('traptools-install')
+exec('oppm install -f traptools ' .. path)
+
+if secondArg == '--reboot' then
+  exec('traptools-install')
+end
+
