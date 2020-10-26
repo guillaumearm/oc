b344402ae6dc1665b270be877d421497d08bee96
Guillaume ARM <trapcodien@gmail.com>
2020-10-26 09:07:19 +0100

    fix: rxlua demo

--- lib/demo/rxlua.lua
+++ lib/demo/rxlua.lua
@@ -1,7 +1,7 @@
 local event = require('event')
 local Rx = require('rx')
 
-Rx.Observable.fromRange(1, 8)
+local sub = Rx.Observable.fromRange(1, 8)
   :filter(function(x) return x % 2 == 0 end)
   :concat(Rx.Observable.of('who do we appreciate'))
   :map(function(value) return value .. '!' end)
