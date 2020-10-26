c0648cdc0f6a3ac611be5f18e0ea1d9e752bb66d
Guillaume ARM <trapcodien@gmail.com>
2020-10-26 14:21:08 +0100

    chore: better rxlua demo

--- lib/demo/rxlua.lua
+++ lib/demo/rxlua.lua
@@ -5,3 +5,8 @@ return Rx.Observable.fromRange(1, 8)
   :concat(Rx.Observable.of('who do we appreciate'))
   :map(function(value) return value .. '!' end)
   :delay(1000)
+  :merge(
+    Rx.Observable.of('hello!'),
+    Rx.Observable.of('bye!'):delay(2000)
+  )
+
