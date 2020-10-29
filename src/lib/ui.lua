abbb83a86c51bb7b798044695c74c7cae2bc5ae8
Guillaume ARM <trapcodien@gmail.com>
2020-10-29 10:16:15 +0100

    chore: add shape to a rendered component

--- src/lib/ui.lua
+++ src/lib/ui.lua
@@ -2,19 +2,24 @@ local api = {}
 
 function getElementDimensions(elem)
   if isString(elem) then
-    return { height=1, width=length(elem) }
+    local width = length(elem)
+    -- `P` means primitive element
+    return { height=1, width=width, shape='P' .. String(width) }
   end
-  return { width=elem.width, height=elem.height }
+  return { width=elem.width, height=elem.height, shape=elem.shape }
 end
 
 local getLineDimensions = reduce(function(state, elem)
   local dim = getElementDimensions(elem)
 
+  local lineShape = state.shape .. dim.shape
+
   return {
     width=state.width + dim.width,
-    height=ternary(state.height < dim.height, dim.height, state.height)
+    height=ternary(state.height < dim.height, dim.height, state.height),
+    shape=lineShape
   }
-end, { width=0, height=0 })
+end, { width=0, height=0, shape='' })
 
 function computeElementDimensions(elem)
   if elem.height and elem.width then return elem end
@@ -25,12 +30,15 @@ function computeElementDimensions(elem)
     line = line.content or line
     local lineDim = getLineDimensions(line)
 
+    -- `|` means end of line
+    state.shape = state.shape .. lineDim.shape .. '|'
     state.height = state.height + lineDim.height
+
     if state.width < lineDim.width then
       state.width = lineDim.width
     end
 
-    return assign({ content=line }, lineDim)
+    return assign({ content=line }, { width=lineDim.width, height=lineDim.height })
   end, elem.content)
 
   return assign(elem, { content=newLines }, state)
