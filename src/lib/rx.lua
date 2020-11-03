1d71c07e585b922e0c3933f5a762ae697f33a379
Guillaume ARM <trapcodien@gmail.com>
2020-11-03 09:16:05 +0100

    fix(rx): add anonymoussubject

--- src/lib/rx.lua
+++ src/lib/rx.lua
@@ -1195,7 +1195,6 @@ function Observable:find(predicate)
   end)
 end
 
-
 --- Returns a new Observable that only produces the first result of the original.
 -- @returns {Observable}
 function Observable:first()
@@ -2635,6 +2634,60 @@ function ReplaySubject:onNext(...)
 end
 
 ReplaySubject.__call = ReplaySubject.onNext
+
+local Subject -- lazy loaded to avoid loop
+local _initialized = false
+
+-- @class AnonymousSubject
+-- @description A specialized Subject which acts as a proxy when lifting a Subject. 
+-- **This is NOT a public class, it is intended for internal use only!**<br>
+-- Its role is crucial to create a proper chain of operators / observables and to make
+-- automatic unsubscription work correctly.
+local AnonymousSubject = {}
+AnonymousSubject.__index = AnonymousSubject
+AnonymousSubject.__tostring = util.constant('AnonymousSubject')
+
+local function lazyInitClass()
+  if _initialized then return end
+  setmetatable(AnonymousSubject, Subject)
+  _initialized = true
+end
+
+function AnonymousSubject.create(sourceSubject, createObserver)
+  lazyInitClass()
+
+  local self = setmetatable(Subject.create(), AnonymousSubject)
+  self._sourceSubject = sourceSubject
+  self._createObserver = createObserver
+
+  return self
+end
+
+function AnonymousSubject:onNext(...)
+  if self._sourceSubject and self._sourceSubject.onNext then
+    self._sourceSubject:onNext(...)
+  end
+end
+
+function AnonymousSubject:onError(msg)
+  if self._sourceSubject and self._sourceSubject.onError then
+    self._sourceSubject:onError(msg)
+  end
+end
+
+function AnonymousSubject:onCompleted()
+  if self._sourceSubject and self._sourceSubject.onCompleted then
+    self._sourceSubject:onCompleted()
+  end
+end
+
+function AnonymousSubject:_subscribe(destination)
+  if self._sourceSubject then
+    return self._sourceSubject:_subscribe(self._createObserver(destination))
+  else
+    return Subscription.EMPTY
+  end
+end
 
 Observable.wrap = Observable.buffer
 Observable['repeat'] = Observable.replicate
