bbaba91b095fd362041e9d11704f6213a98b2fd1
Guillaume ARM <trapcodien@gmail.com>
2020-11-03 19:29:56 +0100

    fix(rx): dump with default formatter

--- src/lib/rx.lua
+++ src/lib/rx.lua
@@ -293,10 +293,13 @@ function Observable:dump(name, formatter)
   name = name and (name .. ' ') or ''
   formatter = formatter or tostring
 
-  local onNext = function(a, ...)
-    a = a  or nil
-    print(name .. 'onNext: ' .. formatter(a, ...))
+  local onNext
+  if formatter then
+    onNext = function(...) print(name .. 'onNext: ' .. formatter(...)) end
+  else
+    onNext = function(...) print(name .. 'onNext: ', ...) end
   end
+
   local onError = function(e) print(name .. 'onError: ' .. e) end
   local onCompleted = function() print(name .. 'onCompleted') end
 
