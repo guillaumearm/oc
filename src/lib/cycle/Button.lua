4198165ad74fbc444856636515f113618d263190
Guillaume ARM <trapcodien@gmail.com>
2020-11-09 17:39:48 +0100

    chore: improve Button

--- src/lib/cycle/Button.lua
+++ src/lib/cycle/Button.lua
@@ -1,18 +1,22 @@
-local Button = function(text, onClick)
+local defaultHighlightedEnhancer = withStyle({ color="black", backgroundColor="white" })
+
+local Button = function(text, onClick, enhancer, highlightedEnhancer)
+  enhancer = enhancer or identity
+  highlightedEnhancer = highlightedEnhancer or defaultHighlightedEnhancer
   text = isObservable(text) and text or of(text)
   onClick = onClick or Subject.create()
 
-  local elem = text:map(View):map(withScopedClick(onClick))
+  local elem = text:map(View):map(withScopedClick(onClick), enhancer)
 
   local highlighted = of(false):concat(mergeAll(
     onClick:mapTo(true),
-    onClick:debounce(1000):mapTo(false)
+    onClick:debounce(200):mapTo(false)
   ))
 
   return combineLatest(highlighted, elem)
     :map(function(h, e)
       if h then
-        return withStyle({ color="black", backgroundColor="white" })(e)
+        return highlightedEnhancer(e)
       end
 
       return e
