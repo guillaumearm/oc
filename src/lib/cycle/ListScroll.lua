311bdd5e3fd5284af556ee9581fa31ac28f30797
Guillaume ARM <trapcodien@gmail.com>
2020-11-09 08:10:16 +0100

    feat(ListScroll): default nbDisplayed to 5

--- src/lib/cycle/ListScroll.lua
+++ src/lib/cycle/ListScroll.lua
@@ -3,7 +3,10 @@ local getList = function(elements, nbDisplayedItems, scrollPosition)
   return take(nbDisplayedItems, scrolledElems)
 end
 
+local DEFAULT_NB_DISPLAYED = 5
+
 local ListScroll = function(elems_, nbDisplayed_, onScroll_)
+  nbDisplayed_ = nbDisplayed_ or of(DEFAULT_NB_DISPLAYED)
   onScroll_ = onScroll_ or NEVER
   local onScrollUp_ = onScroll_:pluck('type'):filter(identical(1)):mapTo('scrollup')
   local onScrollDown_ = onScroll_:pluck('type'):filter(identical(-1)):mapTo('scrolldown')
@@ -29,7 +32,7 @@ local ListScroll = function(elems_, nbDisplayed_, onScroll_)
     :startWith(1)
     :distinctUntilChanged()
 
-  return combineLatest(elems_:startWith({}), nbDisplayed_:startWith(0), scrollPosition_)
+  return combineLatest(elems_, nbDisplayed_, scrollPosition_)
     :map(getList):filter()
 end
 
